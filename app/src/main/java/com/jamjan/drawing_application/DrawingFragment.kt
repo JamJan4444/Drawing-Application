cb68fb284af0c6a2ed6e93526b402dfe73d861a7
Jan Rensing <jan.rensing@gmail.com>
2019-08-15 17:14:01 +0200

    Loading the Image is complete

--- app/src/main/java/com/jamjan/drawing_application/DrawingFragment.kt
+++ app/src/main/java/com/jamjan/drawing_application/DrawingFragment.kt
@@ -1,17 +1,16 @@
 package com.jamjan.drawing_application
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-/**
- * A placeholder fragment containing a simple view.
- */
-class MainActivityFragment : Fragment() {
+class DrawingFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_drawing, container, false)
     }
 }
--- app/src/main/java/com/jamjan/drawing_application/MainActivity.kt
+++ app/src/main/java/com/jamjan/drawing_application/MainActivity.kt
@@ -3,10 +3,22 @@ package com.jamjan.drawing_application
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button
 
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.provider.MediaStore
+import android.widget.ImageView
+
+
+const val PICK_IMAGE = 1
 
 class MainActivity : AppCompatActivity() {
+
+    var buttonImageChooser : Button? = null
+    var bitmap : Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +29,40 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        // Get the button
+        buttonImageChooser = findViewById(R.id.fragment_main_choose_image_button)
+
+        // Add a listener to the button
+        buttonImageChooser?.setOnClickListener{view ->
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select an Image"), PICK_IMAGE)
+        }
     }
 
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // At this point check the result code!
+        if(resultCode == RESULT_OK) {
+            if (requestCode == PICK_IMAGE) {
+
+                // load the bitmap by uri
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data?.data)
+
+                // Switch to edit fragment / drawing fragment
+                setContentView(R.layout.fragment_drawing)
+
+                // Load the bitmap into the fragment
+                findViewById<ImageView>(R.id.fragment_drawing_background_image_view)?.setImageBitmap(bitmap)
+
+            } else {
+                // Not known!
+            }
+        }
+    }
 }
